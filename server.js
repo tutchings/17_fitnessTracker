43ed892dca6ae21bf36717d84f6f62b613985a16
tutchings <tutchings.dev@gmail.com>
2021-01-07 14:26:40 -0500

    built express server and added gitignore

---
+++ server.js
@@ -0,0 +1,25 @@
+// requirement declarations
+const express = require("express");
+const mongoose = require("mongoose");
+
+// global variable declarations
+const PORT = process.env.PORT || 3000
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// connects to database and creates workout db if doesnt exist
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// links to express routes
+app.use(require("./routes/api.js"));
+
+// starts server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
